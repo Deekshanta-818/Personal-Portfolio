7353a4cf0cb01fd1a01de7d6d12086ffc8d429ac
Deekshanta-818 <deekshantapaudel@gmail.com>
2020-12-06 20:07:03 +0545

    My commit

--- js/main.js
+++ js/main.js
@@ -15,3 +15,29 @@ const deg = 6;
                 mn.style.transform = `rotateZ(${mm}deg)`;
                 sc.style.transform = `rotateZ(${ss}deg)`;
             })
+
+            let options = {
+                startAngle: -1.55,
+                size: 150,
+                value: 0.85,
+                fill: {gradient: ['red']}
+              }
+              $(".circle .bar").circleProgress(options).on('circle-animation-progress',
+              function(event, progress, stepValue){
+                $(this).parent().find("span").text(String(stepValue.toFixed(2).substr(2)) + "%");
+              });
+              $(".js .bar").circleProgress({
+                value: 0.50
+              });
+              $(".react .bar").circleProgress({
+                value: 0.60
+              });
+              $(".python .bar").circleProgress({
+                value: 0.50
+              });
+              $(".node .bar").circleProgress({
+                value: 0.65
+              });
+              $(".wp .bar").circleProgress({
+                value: 0.80
+              });
